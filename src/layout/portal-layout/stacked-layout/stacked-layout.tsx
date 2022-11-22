c16edf5b6604f3841c869120d8f5208b861cf7d3
Hung Pham <truonghungit@gmail.com>
2022-11-22 14:01:49 +0700

    ci: add vercel config

--- src/layout/portal-layout/stacked-layout/stacked-layout.tsx
+++ src/layout/portal-layout/stacked-layout/stacked-layout.tsx
@@ -185,11 +185,11 @@ export default function StackedLayout() {
           )}
         </Disclosure>
 
-        <header className='bg-white shadow-sm z-10 flex-shrink-0 flex-grow-0'>
+        {/* <header className='bg-white shadow-sm z-10 flex-shrink-0 flex-grow-0'>
           <div className='max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8'>
             <h1 className='text-lg leading-6 font-semibold text-gray-900'>Nguyễn Văn A</h1>
           </div>
-        </header>
+        </header> */}
 
         <main className='flex-grow overflow-auto'>
           <div className='max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8'>
